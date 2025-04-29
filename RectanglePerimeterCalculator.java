a76841fac0f5105b11d14b35a3e38ef37e152682
Saisreemunjeti <saisreemunjeti@gmail.com>
2025-04-29 19:08:06 +0530

    Add files via upload

---
+++ RectanglePerimeterCalculator.java
@@ -0,0 +1,8 @@
+public class RectanglePerimeterCalculator{
+public static void calculatePerimeter(double length , double breadth){
+System.out.println("Perimeter:" + (2*(length+breadth)));
+}
+public static void main(String [] args){
+calculatePerimeter(20.0,35.0);
+}
+}
---
+++ StringConcatenator.java
@@ -0,0 +1,8 @@
+public class StringConcatenator{
+public static void concatenatorStrings(String str1, String str2){
+System.out.println("Concatenated String:" + (str1 + str2));
+}
+public static void main(String [] args){
+concatenatorStrings("Hello" , "Word");
+}
+}
